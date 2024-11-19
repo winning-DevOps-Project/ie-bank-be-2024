aaf458f160e9faef22cf8aa8e5c210c99a2b6b62
PIETRO <prodrigano.ieu2022@student.ie.edu>
2024-11-19 12:53:54 +0100

    Add database initialization and default admin creation

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -117,3 +117,8 @@ def format_account(account):
         'created_at': account.created_at
     }
 
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  
+        create_default_admin() 
+    app.run(debug=True)
